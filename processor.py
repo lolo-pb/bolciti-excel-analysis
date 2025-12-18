fa7eda1f335420c93b7055c7286c075fd4341167
lolo <lpizzutobeltran@gmail.com>
2025-12-18 14:20:04 -0300

    added blanco y negro y tabla final por secciones

--- processor.py
+++ processor.py
@@ -5,8 +5,9 @@ import pandas as pd
 ## Processing raw input
 
 # Extract data frames from excel files
-sueldos_negros = pd.read_excel("res/sueldos-s2.xlsx", header=5)
-sueldos_blancos = pd.read_excel("res/sueldos-sg.xlsx", header=5)
+sueldos_negros = pd.read_excel("res/sueldos-s2.xlsx", header=5, dtype={"Total": float})
+sueldos_blancos = pd.read_excel("res/sueldos-sg.xlsx", header=5, dtype={"Total": float})
+
 
 # Standardizes colum names
 sueldos_blancos.columns = (
@@ -15,9 +16,22 @@ sueldos_blancos.columns = (
       .str.lower()              # lowercase
       .str.replace(' ', '_')    # snake case >:P
 )
+sueldos_negros.columns = (
+    sueldos_negros.columns
+      .str.strip()              # remove leading/trailing spaces
+      .str.lower()              # lowercase
+      .str.replace(' ', '_')    # snake case >:P
+)
 
 # Only keep the columns i want to work with
 sueldos_blancos = sueldos_blancos[[ "fecha_cierre", "empleado", "total"]]
+sueldos_negros = sueldos_negros[[ "fecha_cierre", "empleado", "total"]]
+
+# Join both black and white sueldos
+sueldos = pd.concat(
+    [sueldos_blancos, sueldos_negros],
+    ignore_index=True
+)
 
 
 
@@ -50,23 +64,31 @@ section_map = {
 
     # General
     "COLINA POLANCO ALAIN ANTONIO": "gral",
+
+    ## Desconocidos caen en gral
+    #"BRIAN NAHUEL SOTO":"",
+    #"BRIZUELA HERNAN":"",
+    #"COLINA POLANCO ALAIN ANTONIO":"",
+    #"LEONARDO LEDESMA":"",
+    #"ROSA BEATRIZ DI PAOLO":"",
+    #"SERGIO DAMIAN ARRIETA ":"",
 }
 # Normalizes names
-sueldos_blancos["empleado"] = (
-    sueldos_blancos["empleado"]
+sueldos["empleado"] = (
+    sueldos["empleado"]
       .str.strip()
       .str.replace(r"\s+", " ", regex=True)
 )
-sueldos_blancos["seccion"] = sueldos_blancos["empleado"].map(section_map).fillna("gral")
+sueldos["seccion"] = sueldos["empleado"].map(section_map).fillna("gral")
 
 
 
 ## Ordering and styling of output table
 # Asign a month by entry
-sueldos_blancos["mes"] = sueldos_blancos["fecha_cierre"].dt.to_period("M")
+sueldos["mes"] = sueldos["fecha_cierre"].dt.to_period("M")
 
 # pivot: one column per month, totals summed
-sueldos_mensuales = sueldos_blancos.pivot_table(
+sueldos = sueldos.pivot_table(
     index=["seccion","empleado"],
     columns="mes",
     values="total",
@@ -76,10 +98,22 @@ sueldos_mensuales = sueldos_blancos.pivot_table(
 
 # order rows by section (custom order) and then employee
 section_order = ["confeccion", "impresion", "extrusion", "echado", "oficina", "gral"]
-sueldos_mensuales["seccion"] = pd.Categorical(
-    sueldos_mensuales["seccion"], categories=section_order, ordered=True
+sueldos["seccion"] = pd.Categorical(
+    sueldos["seccion"], categories=section_order, ordered=True
 )
 
-sueldos_mensuales = sueldos_mensuales.sort_values(["seccion", "empleado"]).reset_index(drop=True)
+#### done
+sueldos = sueldos.sort_values(["seccion", "empleado"]).reset_index(drop=True)
+#print(sueldos.to_string(index=False))
 
-print(sueldos_mensuales.to_string(index=False))
+
+month_cols = sueldos.columns.difference(["seccion", "empleado"])
+
+sueldos_seccion = (
+    sueldos
+      .groupby("seccion", as_index=False, observed=False)[month_cols] 
+      .sum()
+)#observed incluye categorias que no aparecen
+
+print(sueldos_seccion.to_string(index=False))
+

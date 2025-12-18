b959ab571aa9f3bc4b66f38ed660eb3064f57fec
lolo <lpizzutobeltran@gmail.com>
2025-12-18 16:28:59 -0300

    refectoring done

--- processor.py
+++ processor.py
@@ -1,3 +1,10 @@
+from modules.sueldos import build_sueldos_by_section
+
 ###  Esto deberia llamar a los modules y cada module deberia devolver las 
 ### tablas divididas por mes/ o dividirlas en el main?
+def main():
+        sueldos = build_sueldos_by_section("res/sueldos-sg.xlsx", "res/sueldos-s2.xlsx")
+        print(sueldos.to_string(index=False))
 
+if __name__ == "__main__":
+    main()

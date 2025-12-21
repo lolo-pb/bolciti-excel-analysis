a2154c93979b992e28361038a9a3c97f9eb72e98
lolo <lpizzutobeltran@gmail.com>
2025-12-21 17:33:21 -0300

    done

--- processor.py
+++ processor.py
@@ -6,25 +6,23 @@ from modules.helpers import join_pivots
 ###  Esto deberia llamar a los modules y cada module deberia devolver las 
 ### tablas divididas por mes/ o dividirlas en el main?
 def main():
-        #sueldos = build_sueldos_by_section()
+        sueldos = build_sueldos_by_section()
         #print(sueldos.to_string(index=False))
 
         #print("=====================================================================")
 
-        #gastos = build_gastos_by_section()
+        gastos = build_gastos_by_section()
         #print(gastos.to_string(index=False))
 
         #print("=====================================================================")
         #facturas = build_facturas_por_cliente()
         facturas =build_facturas_total()
-        print(facturas.to_string(index=False))
-        #print(facturas.head())
-        #print(facturas.dtypes)
-
+        #print(facturas.to_string(index=False))
+        
         #print("=====================================================================")
 
-        #resumen = join_pivots(gastos,sueldos)
-        #print(resumen.to_string(index=False))
+        resumen = join_pivots(gastos,sueldos)
+        print(resumen.to_string(index=False))
 
 if __name__ == "__main__":
     main()
